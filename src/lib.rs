09eaf3954091f88c4e3713f1b5990d85c2cf14b1
whatwareweb <59752545+whatwareweb@users.noreply.github.com>
2025-07-07 10:47:57 -0500

    i don't even know anymore

---
+++ src/lib.rs
@@ -0,0 +1,100 @@
+use std::f32::consts::PI;
+
+#[cfg(feature = "wasm")]
+use wasm_bindgen::prelude::*;
+
+#[cfg(feature = "wasm")]
+use web_sys::console;
+
+use cpal::{SizedSample, FromSample};
+
+#[cfg(feature = "wasm")]
+#[wasm_bindgen(start)]
+pub fn main_js() -> Result<(), JsValue> {
+    // provides better error messages in debug
+    // disabled in release mode as to not bloat the file size
+    #[cfg(debug_assertions)]
+    console_error_panic_hook::set_once();
+
+    Ok(())
+}
+
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use cpal::Stream;
+
+#[cfg_attr(feature = "wasm", wasm_bindgen)]
+pub struct Handle(Stream);
+
+#[cfg_attr(feature = "wasm", wasm_bindgen)]
+pub fn tone() -> Handle {
+    let host = cpal::default_host();
+    let device = host.default_output_device().expect("failed to find output device");
+    let config = device.default_output_config().unwrap();
+    Handle(match config.sample_format() {
+        cpal::SampleFormat::F32 => run::<f32>(&device, &config.into()),
+        cpal::SampleFormat::I16 => run::<i16>(&device, &config.into()),
+        cpal::SampleFormat::U16 => run::<u16>(&device, &config.into()),
+        _ => panic!("sample format unsupported"),
+    })
+}
+
+fn sin_sample(sample_clock: f32, sample_rate: f32, freq: f32) -> f32 {
+    (2.0 * (sample_clock * freq * PI / sample_rate)).sin()
+}
+
+fn sqr_sample(sample_clock: f32, sample_rate: f32, freq: f32) -> f32 {
+    (2.0 * (sample_clock * freq * PI / sample_rate)).sin().signum()
+}
+
+fn tri_sample(sample_clock: f32, sample_rate: f32, freq: f32) -> f32 {
+    (2.0 * ((2.0 * (((sample_clock * freq * PI / sample_rate) - PI / 4.0) % PI) - PI) / PI)).abs() - 1.0
+}
+
+fn saw_sample(sample_clock: f32, sample_rate: f32, freq: f32) -> f32 {
+    (2f32 * ((sample_clock * freq * PI / sample_rate) % PI) - PI) / PI
+}
+
+fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig) -> Stream
+where
+    T: SizedSample + FromSample<f32>,
+{
+    let sample_rate = config.sample_rate.0 as f32;
+    let channels = config.channels as usize;
+
+    // produce fm sin wave (wow fancy)
+        let mut sample_clock = 0f32;
+    let mut next_value = move || {
+        sample_clock = (sample_clock + 1.0) % sample_rate;
+        sin_sample(sample_clock, sample_rate, 440.0 + 6.0 * sin_sample(sample_clock + 20.0, sample_rate, 440.0))
+    };
+
+    #[cfg(feature = "native")]
+    let err_fn = |err| eprintln!("{}", &format!("an error occurred on stream: {}", err));
+
+    #[cfg(not(feature = "native"))]
+    let err_fn = |err| console::error_1(&format!("an error occurred on stream: {}", err).into());
+
+
+    let stream = device
+        .build_output_stream(
+            config,
+            move |data: &mut [T], _| write_data(data, channels, &mut next_value),
+            err_fn,
+            None,
+        )
+        .unwrap();
+    stream.play().unwrap();
+    stream
+}
+
+fn write_data<T>(output: &mut [T], channels: usize, next_sample: &mut dyn FnMut() -> f32)
+where
+    T: SizedSample + FromSample<f32>,
+{
+    for frame in output.chunks_mut(channels) {
+        let value: T = T::from_sample(next_sample());
+        for sample in frame.iter_mut() {
+            *sample = value;
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+#[cfg(not(feature = "wasm"))]
+fn main() {
+    println!("Hello, world!");
+    let handle = limid::tone();
+    loop {
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    }
+}
